3797093cd7df6873adbc0d61e0c6bcdd9ba8ab11
isu5 <573301558@qq.com>
2019-01-07 15:01:49 +0800

    修复企业产品无法入库

--- Application/Home/Model/ProductModel.class.php
+++ Application/Home/Model/ProductModel.class.php
@@ -42,8 +42,30 @@ class ProductModel extends BaseModel{
 	}
 	
 	//前端搜索
-	public function searchFront(){
+	public function searchFront($pagesize=15){
+		$where['uid'] = cookie(userid);
+		//翻页
+		$count = $this->where($where)->count();
+		$page = new \Think\Page($count,$pagesize);
+		//配置分页
+		$page->setConfig('prev', '上一页');
+		$page->setConfig('next', '下一页');
+		$data['page'] = $page->show();
+		$data['data'] = $this
+		->where($where)
+		->limit($page->firstRow.','.$page->listRows)
+		->order('id desc')
+		->select();
 		
+		return $data;
+	}
+	
+	
+	//添加前
+	public function _before_insert(&$data, $option){
+		$data['addtime'] = time();
+		$data['uid'] = cookie(userid);
+		$data['pic'] = json_encode($_POST['pic']);
 	}
 	
 	
